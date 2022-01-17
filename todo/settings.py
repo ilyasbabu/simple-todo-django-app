035b2380e45cfe394a5e32f4e9b9ef51a52dbbd6
ilyasbabu <ilyasbabuvp@gmail.com>
2022-01-17 17:30:26 +0530

    Automatic configuration with django-heroku

--- todo/settings.py
+++ todo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
