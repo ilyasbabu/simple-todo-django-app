8afca27a2e6aaa74a4fae3b1001318090618753e
ilyasbabu <ilyasbabuvp@gmail.com>
2022-01-17 14:44:32 +0530

    class based view completed

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,7 +2,13 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', view, name='view'),
+    # path('', view, name='view'),          #function based view
+    path('',TaskListView.as_view(),name='TaskListView'),  #generic class based view
+    path('add/', add, name='add'),  
     path('done/<int:task_id>',done,name='done'),
     path('update/<int:task_id>',update,name="update"),
+    path('<pk>/',TaskDetailView.as_view(),name='TaskDetailView'),
+    path('<pk>/update/',TaskUpdateView.as_view(),name='TaskUpdateView'),
+    path('<pk>/delete/',TaskDeleteView.as_view(),name='TaskDeleteView'),
+    
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,27 +1,70 @@
+from asyncio import tasks
+from dataclasses import field, fields
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from .models import Task
 from .form import Taskform
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
-def view(request):
-    task=Task.objects.all().order_by('-id')
-    context={
-        'task':task
-    }
+class TaskListView(ListView):
+    model=Task
+    template_name='task_view.html'
+    context_object_name='task'
+
+class TaskDetailView(DetailView):
+    model=Task
+    template_name='task_detail.html'
+    context_object_name='task'
+
+class TaskUpdateView(UpdateView):
+    model=Task
+    template_name='task_detail.html'
+    context_object_name='form'
+    fields=['title','priority']
+    success_url='/'
+    # def get_success_url(self):
+    #     return reverse_lazy('',kwargs={'pk':self.object.id})
+
+class TaskDeleteView(DeleteView):
+    model=Task
+    template_name='confirm.html'
+    context_object_name='task'
+    success_url=reverse_lazy('TaskListView')
+
+#function based view to list out all the tasks
+#_______________________________________________
+# def view(request):
+#     task=Task.objects.all().order_by('-id')
+#     context={
+#         'task':task
+#     }
+#     if request.method=="POST":
+#         title=request.POST['title']
+#         priority=request.POST['priority']
+#         task=Task(title=title,priority=priority)
+#         task.save()
+#         return redirect('/')
+#     return render(request,'task_view.html',context)
+
+def add(request):
     if request.method=="POST":
         title=request.POST['title']
         priority=request.POST['priority']
         task=Task(title=title,priority=priority)
         task.save()
         return redirect('/')
-    return render(request,'task_view.html',context)
+    return render(request,'task_view.html')
 
 def done(request,task_id):
     tasks=Task.objects.get(id=task_id)
     task=Task.objects.all().order_by('-id')
-    if request.method=="POST":
+    if request.method=="GET":
         tasks.delete()
         return redirect('/')
     return render(request,'task_view.html',{'task':task})
